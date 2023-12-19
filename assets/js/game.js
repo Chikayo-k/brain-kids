ba5deb62ea89003a8acfcb1eb10b1219050f7993
Chika <chika000chika@gmail.com>
2023-12-19 11:39:26 +0000

    Add code to show level 0 when the page load

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,18 @@
 //Add background image
 document.body.style.background =
   'url("../assets/images/puzzle.png") center center/cover';
+
+//Math game
+
+let start = "false";
+let level = 0;
+
+// Show level 0 when the page load and set start true
+
+window.addEventListener("load", function () {
+  if (start) {
+    let level_place = document.getElementsByClassName("level")[0];
+    level_place.innerHTML = `Level ${level}`;
+    start = true;
+  }
+});
