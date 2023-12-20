1139ed1edfc8f5b8c62a3cf472f48d44821198fb
Chika <chika000chika@gmail.com>
2023-12-20 09:14:18 +0000

    Add code to start a game and display questions

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,10 +2,11 @@
 document.body.style.background =
   'url("../assets/images/puzzle.png") center center/cover';
 
-//Math game
+// //Math game
 
 let start = "false";
 let level = 0;
+let count = 0;
 
 // Show level 0 when the page load and set start true
 
@@ -14,5 +15,49 @@ window.addEventListener("load", function () {
     let level_place = document.getElementsByClassName("level")[0];
     level_place.innerHTML = `Level ${level}`;
     start = true;
+    games_starts(level);
   }
 });
+
+/**
+ * Game stars
+ */
+function games_starts(level) {
+  let num1 = Math.floor(Math.random() * 10);
+  let num2 = Math.floor(Math.random() * 10);
+
+  //  Render additional questions and subtraction questions three times each.
+  //  After that the level will be up.
+
+  if (count < 3) {
+    show_addition(num1, num2);
+
+    count++;
+  } else if (count < 6) {
+    show_subtraction(num1, num2);
+    count++;
+  } else {
+    console.log("Next Level");
+    level++;
+    // next_level(level);
+  }
+  console.log(level);
+}
+
+/**
+ * Display additional question
+ */
+function show_addition(num1, num2) {
+  document.getElementById("num1").innerHTML = num1;
+  document.getElementById("num2").innerHTML = num2;
+  document.getElementById("operator").innerHTML = "+";
+}
+
+/**
+ * Display subtraction question
+ */
+function show_subtraction(num1, num2) {
+  document.getElementById("num1").innerHTML = num1 > num2 ? num1 : num2;
+  document.getElementById("num2").innerHTML = num2 < num1 ? num2 : num1;
+  document.getElementById("operator").innerHTML = "-";
+}
